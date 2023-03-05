60758771bffc9043f615dc32ec8075ec645d0ad1
Silverdog <c@wakko.one>
2023-03-05 22:38:02 +1100

    home view modified

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,9 +14,7 @@
  <v-card title="ABOUT" class="ma-4 bg-teal-darken-1">
   <v-card-text>
     Please be aware that this software is licensed under GPL-v2, which specifies the terms and conditions of its use. You are welcome to suggest updates or new features, which I will gladly review and commit.
-    <a href="https://github.com/Wakk0/fool-toll-tool"><v-img width="100px" src="src/assets/github_PNG15.png"/></a>
-    Sources are available in my abandoned GitHub account x-D
-    
+    <a href="https://github.com/Wakk0/fool-toll-tool">Sources are available in my abandoned GitHub account x-D</a>
   </v-card-text>
  </v-card>
 </template>
